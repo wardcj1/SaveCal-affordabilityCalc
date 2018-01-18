31f7d36a463244bb0b9b878a4faa58965230f038
chris ward <ward.christopherj@gmail.com>
2018-01-18 09:46:44 -0800

    C4 and B5 cells calculated

--- public/app.js
+++ public/app.js
@@ -1,11 +1,39 @@
-#projectAmount
-#monthlySavings
-#rateIncrease
+// #projectAmount
+// #monthlySavings
+// #rateIncrease
+var C4Result;
+var B5Result;
+var C5Result;
+var D5Result;
+var E5Result;
+var B6Result;
+var C6Result;
+var D6Result;
+var E6Result;
+var B7Result;
+var C7Result;
+var D7Result;
+var E7Result;
+var B8Result;
+var C8Result;
+var D8Result;
+var E8Result;
+var B9Result;
+var C9Result;
+var D9Result;
+var E9Result;
+var B10Result;
+var C10Result;
+var D10Result;
+var E10Result;
+ 
+  $("#target").submit(function() {
 
-var = C4formula
+    $("#C4").text(C4Result);
+    C4Result = "$ " + ($("#projectAmount").val() * 0.0033).toFixed(2);
+    
+    $("#B5").text(B5Result);
+    B5Result = "$ " + ($("#projectAmount").val() * 0.01666667).toFixed(2);    
 
-
-function promoPeriodPayments(){
-document.getElementById('C4formula').innerHTML = "Hello";
-// projectAmount*0.0033;
-}
+   event.preventDefault();
+  });

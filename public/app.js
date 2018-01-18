2ea8007ee46635faad349ab8baeb5637c6a161cb
chris ward <ward.christopherj@gmail.com>
2018-01-18 01:18:31 -0800

    intro js

--- public/app.js
+++ public/app.js
@@ -1,3 +1,11 @@
 #projectAmount
 #monthlySavings
 #rateIncrease
+
+var = C4formula
+
+
+function promoPeriodPayments(){
+document.getElementById('C4formula').innerHTML = "Hello";
+// projectAmount*0.0033;
+}

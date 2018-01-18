d4a0ded5a9dc560f1bfa5811c112ac2672a2a67e
chris ward <ward.christopherj@gmail.com>
2018-01-18 09:54:41 -0800

    cells calculated template

--- public/app.js
+++ public/app.js
@@ -29,11 +29,88 @@ var E10Result;
  
   $("#target").submit(function() {
 
+  	// Row 4 
     $("#C4").text(C4Result);
     C4Result = "$ " + ($("#projectAmount").val() * 0.0033).toFixed(2);
     
+   	// Row 5   
     $("#B5").text(B5Result);
     B5Result = "$ " + ($("#projectAmount").val() * 0.01666667).toFixed(2);    
+
+    $("#C5").text(C5Result);
+    C5Result = "$ " + ($("#projectAmount").val() * 0.0097).toFixed(2);
+
+    $("#D5").text(D5Result);
+    D5Result = "$ " + ($("#projectAmount").val() * 0.01077).toFixed(2);     
+
+    $("#E5").text(E5Result);
+    E5Result = "$ " + ($("#projectAmount").val() * 0.0157).toFixed(2);
+
+    // Row 6   
+    // $("#B5").text(B5Result);
+    // B5Result = "$ " + ($("#projectAmount").val() * 0.01666667).toFixed(2);    
+
+    // $("#C5").text(C5Result);
+    // C5Result = "$ " + ($("#projectAmount").val() * 0.0097).toFixed(2);
+
+    // $("#D5").text(D5Result);
+    // D5Result = "$ " + ($("#projectAmount").val() * 0.01077).toFixed(2);     
+
+    // $("#E5").text(E5Result);
+    // E5Result = "$ " + ($("#projectAmount").val() * 0.0157).toFixed(2); 
+
+    // Row 7   
+    // $("#B5").text(B5Result);
+    // B5Result = "$ " + ($("#projectAmount").val() * 0.01666667).toFixed(2);    
+
+    // $("#C5").text(C5Result);
+    // C5Result = "$ " + ($("#projectAmount").val() * 0.0097).toFixed(2);
+
+    // $("#D5").text(D5Result);
+    // D5Result = "$ " + ($("#projectAmount").val() * 0.01077).toFixed(2);     
+
+    // $("#E5").text(E5Result);
+    // E5Result = "$ " + ($("#projectAmount").val() * 0.0157).toFixed(2); 
+
+    // Row 8   
+    // $("#B5").text(B5Result);
+    // B5Result = "$ " + ($("#projectAmount").val() * 0.01666667).toFixed(2);    
+
+    // $("#C5").text(C5Result);
+    // C5Result = "$ " + ($("#projectAmount").val() * 0.0097).toFixed(2);
+
+    // $("#D5").text(D5Result);
+    // D5Result = "$ " + ($("#projectAmount").val() * 0.01077).toFixed(2);     
+
+    // $("#E5").text(E5Result);
+    // E5Result = "$ " + ($("#projectAmount").val() * 0.0157).toFixed(2); 
+
+    // Row 9   
+    // $("#B5").text(B5Result);
+    // B5Result = "$ " + ($("#projectAmount").val() * 0.01666667).toFixed(2);    
+
+    // $("#C5").text(C5Result);
+    // C5Result = "$ " + ($("#projectAmount").val() * 0.0097).toFixed(2);
+
+    // $("#D5").text(D5Result);
+    // D5Result = "$ " + ($("#projectAmount").val() * 0.01077).toFixed(2);     
+
+    // $("#E5").text(E5Result);
+    // E5Result = "$ " + ($("#projectAmount").val() * 0.0157).toFixed(2); 
+
+    // Row 10   
+    // $("#B5").text(B5Result);
+    // B5Result = "$ " + ($("#projectAmount").val() * 0.01666667).toFixed(2);    
+
+    // $("#C5").text(C5Result);
+    // C5Result = "$ " + ($("#projectAmount").val() * 0.0097).toFixed(2);
+
+    // $("#D5").text(D5Result);
+    // D5Result = "$ " + ($("#projectAmount").val() * 0.01077).toFixed(2);     
+
+    // $("#E5").text(E5Result);
+    // E5Result = "$ " + ($("#projectAmount").val() * 0.0157).toFixed(2);      
+
 
    event.preventDefault();
   });
